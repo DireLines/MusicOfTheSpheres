ff9c94e8f6db9cd2ef0e199393a70f366cbf197b
Nathaniel Saxe <NathanielSaxophone@gmail.com>
2020-05-16 21:18:18 -0400

    fog.

---
+++ MusicOfTheSpheres/Assets/Resources/Scripts/Visual/FogPlane.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FogPlane : MonoBehaviour {
+    Transform mainCam;
+
+    [SerializeField]
+    float offset;
+
+    private void Start() {
+        mainCam = Camera.main.transform;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        transform.position = mainCam.position + new Vector3(0, offset, 0);
+    }
+}
