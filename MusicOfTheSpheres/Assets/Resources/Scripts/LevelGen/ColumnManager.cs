d20085b14f3bfb684d8d86fbc923c1905b00d641
Nathaniel Saxe <NathanielSaxophone@gmail.com>
2020-06-11 18:50:18 -0400

    it's janky but I'm not sure why

--- MusicOfTheSpheres/Assets/Resources/Scripts/LevelGen/ColumnManager.cs
+++ MusicOfTheSpheres/Assets/Resources/Scripts/LevelGen/ColumnManager.cs
@@ -59,6 +59,7 @@ public class ColumnManager : MonoBehaviour {
         newColumn.GetComponent<Column>().playNotes = playNotes;
         //TODO: set platform height to note
         newColumn.transform.Find("Platform").GetComponent<Platform>().height = note;
+        newColumn.transform.Find("Stairs").Find("StairStep").GetComponent<Platform>().height = note + 1;
 
         //TODO: use this machine spawning logic once machine prefab is done
         //for (int i = 0; i < UnityEngine.Random.Range(1, 5); i++) {
--- MusicOfTheSpheres/Assets/Resources/Scripts/Player/PlayerController.cs
+++ MusicOfTheSpheres/Assets/Resources/Scripts/Player/PlayerController.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
@@ -15,9 +16,9 @@ public class PlayerController : MonoBehaviour {
     private float moveSpeed;
 
     const float reallyBig = 9000;
-    const float reallySmall = 0.0001f;
+    const float reallySmall = 0.001f;
 
-    private Platform currentPlatform;
+    private Transform currentPlatform;
 
     // Start is called before the first frame update
     void Start() {
@@ -29,8 +30,9 @@ public class PlayerController : MonoBehaviour {
     void FixedUpdate() {
         if (!currentPlatform) {
             ColumnManager CM = GameObject.Find("LevelGenerator").GetComponent<ColumnManager>();
-            currentPlatform = CM.columnsInGrid[CM.PlayerGridSquare()].GetComponentInChildren<Platform>();
+            currentPlatform = CM.columnsInGrid[CM.PlayerGridSquare()].transform.Find("Platform");
         }
+        print(currentPlatform.GetComponent<Platform>().height);
         Vector3 forward = Vector3.ProjectOnPlane(Camera.main.transform.forward, Vector3.up).normalized;
         Vector3 right = Vector3.ProjectOnPlane(Camera.main.transform.right, Vector3.up).normalized;
         Vector2 movementWithinPlane = new Vector2(Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime, Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime);
@@ -49,23 +51,32 @@ public class PlayerController : MonoBehaviour {
             float.MaxValue,
             1 << LayerMask.NameToLayer("Platform")
         );
-        if (hits.Length < 1) {
-            GetComponent<Rigidbody>().velocity = dir + new Vector3(0, GetComponent<Rigidbody>().velocity.y, 0);
-            return;
+        hits = hits.OrderBy(hit => -hit.transform.GetComponent<Platform>().height).ToArray();
+        print("hits:");
+        foreach (RaycastHit hit in hits) {
+            print(hit.transform.gameObject.name);
         }
-        Transform platform = hits[0].transform;
-        if (platform == currentPlatform.transform) {
-            GetComponent<Rigidbody>().velocity = dir + new Vector3(0, GetComponent<Rigidbody>().velocity.y, 0);
-            return;
+        print("current platform: " + currentPlatform.gameObject.name);
+        if (hits.Length > 0) {
+            Transform platform = hits[0].transform;
+            //print(currentPlatform.gameObject.name + " -> " + platform.gameObject.name);
+            if (platform != currentPlatform) {
+                Platform p = platform.GetComponent<Platform>();
+                if (Mathf.Abs(p.height - currentPlatform.GetComponent<Platform>().height) <= 1 + reallySmall) {
+                    //print("success!");
+                    Vector3 destination = new Vector3(heading.x, transform.localScale.y / 2 + platform.position.y + platform.localScale.y / 2 + reallySmall, heading.z);
+                    transform.position = destination;
+                    currentPlatform = platform;
+                    return;
+                } else {
+                    //print("too big a gap");
+                }
+            } else {
+                //print("same platform");
+            }
+        } else {
+            //print("no raycast hits");
         }
-        Platform p = platform.GetComponent<Platform>();
-        if (Mathf.Abs(p.height - currentPlatform.height) > 1) {
-            GetComponent<Rigidbody>().velocity = dir + new Vector3(0, GetComponent<Rigidbody>().velocity.y, 0);
-            return;
-        }
-        Vector3 destination = new Vector3(heading.x, transform.localScale.y / 2 + platform.position.y + platform.localScale.y / 2 + reallySmall, heading.z);
-        print(destination);
-        transform.position = destination;
-        currentPlatform = p;
+        GetComponent<Rigidbody>().velocity = dir + new Vector3(0, GetComponent<Rigidbody>().velocity.y, 0);
     }
 }
