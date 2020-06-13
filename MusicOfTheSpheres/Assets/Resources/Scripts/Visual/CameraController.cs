47530e737ec39d4009377f8b5b072a7557a5b375
ryanakann <ryanakann@gmail.com>
2020-06-12 21:58:35 -0400

    Camera!

---
+++ MusicOfTheSpheres/Assets/Resources/Scripts/Visual/CameraController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+    //Rotation
+    [SerializeField]
+    float turnSpeed, vertSpeed, offsetDistance;
+    [SerializeField]
+    bool turnInverted, vertInverted;
+    float turnDirection, vertDirection;
+
+    //Zoom
+    [SerializeField]
+    float zoomMax, zoomMin, zoomSensitivity;
+    float zoom, zoomSpeed;
+
+    //Positioning
+    [SerializeField]
+    Transform target;
+    public Transform Target { get => target; set => target = value; }
+
+    [SerializeField]
+    float lerpPercentage = 0.01f;
+
+    private Vector3 targetPos;
+    Vector3 velocityRef;
+    private float yaw, pitch;
+
+
+    void Start() {
+        turnDirection = turnInverted ? -1 : 1;
+        vertDirection = vertInverted ? -1 : 1;
+        targetPos = target.position;
+
+        yaw = 0f;
+        pitch = 0f;
+    }
+    void LateUpdate() {
+        //Rotation
+        yaw += turnSpeed * turnDirection * Input.GetAxis("Camera Horizontal") * Time.deltaTime;
+        pitch += vertSpeed * vertDirection * Input.GetAxis("Camera Vertical") * Time.deltaTime;
+        pitch = Mathf.Clamp(pitch, 5, 80);
+        transform.eulerAngles = new Vector3(pitch, yaw);
+
+        //Zoom
+        zoom += Input.mouseScrollDelta.y * zoomSensitivity * 100f * Time.deltaTime;
+        zoom = Mathf.Clamp(zoom, zoomMin, zoomMax);
+
+        //Positioning
+        targetPos = Vector3.SmoothDamp(targetPos, Target.position, ref velocityRef, lerpPercentage);
+        transform.position = targetPos - transform.forward * zoom;
+    }
+}
