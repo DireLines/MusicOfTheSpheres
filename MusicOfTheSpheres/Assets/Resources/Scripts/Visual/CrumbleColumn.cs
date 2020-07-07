ec2bfa28a5744c75ea0bfdbdb00af12285048497
Nathaniel Saxe <NathanielSaxophone@gmail.com>
2020-07-07 19:40:20 -0400

    update todo

--- MusicOfTheSpheres/Assets/Resources/Scripts/Visual/CrumbleColumn.cs
+++ MusicOfTheSpheres/Assets/Resources/Scripts/Visual/CrumbleColumn.cs
@@ -11,34 +11,34 @@ public class CrumbleColumn : MonoBehaviour {
     private Transform chunks;
 
     //example usage
-    //private float crumbleHeight;
+    private float crumbleHeight;
 
     private void Awake() {
-        //crumbleHeight = 0;
+        crumbleHeight = -15;
         chunks = transform.Find("Chunks");
-        foreach (Transform piece in chunks) {
-            if (piece.GetComponent<Rigidbody>() != null) piece.GetComponent<Rigidbody>().useGravity = false;
+        foreach (Transform chunk in chunks) {
+            if (chunk.GetComponent<Rigidbody>() != null) chunk.GetComponent<Rigidbody>().useGravity = false;
         }
         //rotate to a random right angle for visual variety
         transform.Rotate(Vector3.up, 90f * Random.Range(0, 4));
     }
 
     private void Update() {
-        //if (Input.GetKey(KeyCode.Space)) {
-        //crumbleHeight += Time.deltaTime / crumbleSeconds * 256;
-        //}
-        //CrumbleBelow(crumbleHeight);
+        if (Input.GetKey(KeyCode.Space)) {
+            crumbleHeight += Time.deltaTime / crumbleSeconds * 256;
+        }
+        CrumbleBelow(crumbleHeight);
     }
 
     public void CrumbleBelow(float height) {
-        foreach (Transform piece in chunks) {
-            Rigidbody rb = piece.GetComponent<Rigidbody>();
+        foreach (Transform chunk in chunks) {
+            Rigidbody rb = chunk.GetComponent<Rigidbody>();
             if (!rb) continue;
-            if (piece.position.y <= height && !rb.useGravity) {
+            if (chunk.position.y <= height && !rb.useGravity) {
                 rb.useGravity = true;
                 rb.angularDrag = 0f;
                 rb.angularVelocity = Random.insideUnitSphere * 0.3f;
-                Destroy(piece.gameObject, 20f);
+                Destroy(chunk.gameObject, 20f);
             }
         }
     }
