4c032c6be715a8403830706337887a9d925c9e83
Nathaniel Saxe <NathanielSaxophone@gmail.com>
2020-07-07 22:17:09 -0400

    remove column demo code

--- MusicOfTheSpheres/Assets/Resources/Scripts/Visual/CrumbleColumn.cs
+++ MusicOfTheSpheres/Assets/Resources/Scripts/Visual/CrumbleColumn.cs
@@ -10,11 +10,7 @@ public class CrumbleColumn : MonoBehaviour {
 
     private Transform chunks;
 
-    //example usage
-    private float crumbleHeight;
-
     private void Awake() {
-        crumbleHeight = -15;
         chunks = transform.Find("Chunks");
         foreach (Transform chunk in chunks) {
             if (chunk.GetComponent<Rigidbody>() != null) chunk.GetComponent<Rigidbody>().useGravity = false;
@@ -22,14 +18,6 @@ public class CrumbleColumn : MonoBehaviour {
         //rotate to a random right angle for visual variety
         transform.Rotate(Vector3.up, 90f * Random.Range(0, 4));
     }
-
-    private void Update() {
-        if (Input.GetKey(KeyCode.Space)) {
-            crumbleHeight += Time.deltaTime / crumbleSeconds * 256;
-        }
-        CrumbleBelow(crumbleHeight);
-    }
-
     public void CrumbleBelow(float height) {
         foreach (Transform chunk in chunks) {
             Rigidbody rb = chunk.GetComponent<Rigidbody>();
